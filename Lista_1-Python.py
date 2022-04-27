d0ad328e1096839ecadb49b1b0b5e959c6d267c7
antoniormacedo <94052414+antoniormacedo@users.noreply.github.com>
2022-04-27 20:44:01 -0300

    Update Lista_1-Python.py

--- Lista_1-Python.py
+++ Lista_1-Python.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Exercício 1 item (f) da Lista 1 de Macroeconomia I (Parte 1) de 2022
-26/04/2022 - author: Antonio Ricciardi Macedo
+27/04/2022 - author: Antonio Ricciardi Macedo
 """
 
 # Módulos a serem utilizados
@@ -13,30 +13,28 @@ beta_2 = 0.95  # valor de beta_2 fixado
 # Criando e preenchendo matriz com beta_1 e t*
 tabela = np.zeros([18, 2])  # criando matriz de zeros 18 x 2 para preenchimento
 
-
 # Loop para preenchimento de possíveis \beta_1 < \beta_2 e t* para c1_t - c2_t
 # mudar de sinal (quando c2_t > 1, pois c2_t = 2 - c1_t, no equilíbrio)
 for i in range(len(tabela)):
     tabela[i, 0] = beta_2 - (i + 1) * 0.05
     
-    # Estabelecendo valores iniciais
+    # Calcular consumo inicial do indivíduo 2 (c2_0)
     t = 0
-    c2_t = -np.inf  # valor arbitrário apenas para ser TRUE ao entrar no loop
+    c2_t = - np.inf # um valor pequeno arbitrário para entrar no loop
     
     while c2_t < 1:
         # Como c1_t + c2_t = 2, só precisamos verificar se c2_t > 1 ou c1_t < 1
-        c2_t = 2 / (1 + ((1 - tabela[i, 0]) / (1 - beta_2)) * (tabela[i, 0] / beta_2)**t)
+        c2_t = 2 / (1 + ((1 - tabela[i,0]) / (1 - beta_2)) * (tabela[i,0] / beta_2) ** t)
         t += 1
-    tabela[i, 1] = t
+    tabela[i, 1] = t - 1 # tem que ser t-1 pois o Python não atualiza
+    # automaticamente o valor de c2_t
 
 print(tabela)
-
 
 # Poderia ter usado linspace() do NumPy para preencher a 1ª coluna de uma vez
 exemplo = np.zeros((18, 2))
 exemplo[:, 0] = np.linspace(0.05, 0.90, 18)
 exemplo
-
 
 # Criação do gráfico
 fig, ax = plt.subplots()  # Cria a base (em branco) do gráfico
